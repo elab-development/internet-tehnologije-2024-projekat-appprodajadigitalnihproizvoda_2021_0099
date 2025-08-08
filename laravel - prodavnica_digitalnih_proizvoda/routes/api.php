d33d89db3f60571270fc9d9dd87bd475af83093f
Aleksandar Ljubic <aleksandar.ljbc@gmail.com>
2025-08-09 00:25:48 +0200

    Dodate zasticene rute za admina

--- laravel - prodavnica_digitalnih_proizvoda/routes/api.php
+++ laravel - prodavnica_digitalnih_proizvoda/routes/api.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\RecipeController;
 use App\Http\Controllers\API\AuthController;
+use App\Http\Middleware\IsAdmin;
 
 //Rute za korisnike
 Route::get('/users', [UserController::class, 'index']);
@@ -15,12 +16,15 @@ Route::put('/users/{id}', [UserController::class, 'update']);
 Route::delete('/users/{id}', [UserController::class, 'destroy']);
 
 //Rute za kategorije
-Route::get('/categories', [CategoryController::class, 'index']);
-Route::get('/categories/{id}', [CategoryController::class, 'show']);
+Route::middleware(['auth:sanctum', IsAdmin::class])->group(function () {
+
 Route::post('/categories', [CategoryController::class, 'store']);
 Route::put('/categories/{id}', [CategoryController::class, 'update']);
-Route::delete('/categories/{id}', [CategoryController::class, 'destroy']);  
-
+Route::delete('/categories/{id}', [CategoryController::class, 'destroy']); 
+});
+//Rute za prikaz kategorija koje ce sluziti za filtriranje na frontu
+Route::get('/categories', [CategoryController::class, 'index']);
+Route::get('/categories/{id}', [CategoryController::class, 'show']);
 //Rute za recepte
 Route::get('/recipes', [RecipeController::class, 'index']);
 Route::get('/recipes/{id}', [RecipeController::class, 'show']);
@@ -31,4 +35,5 @@ Route::delete('/recipes/{id}', [RecipeController::class, 'destroy']);
 //Rute za autentifikaciju
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
+//mora ruta da bude zasticena jer samo prijavljeni korisnici mogu da se odjave
 Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
